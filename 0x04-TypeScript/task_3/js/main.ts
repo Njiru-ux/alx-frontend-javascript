8e53e02f85e7c525c05a48304e9aad705a37ef16
Njiru-ux <ericbishop99@gmail.com>
2025-09-22 03:09:16 +0300

    Add exact pattern { ...row, age: 23 } for updatedRow

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -9,7 +9,9 @@ const row: RowElement = {
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
-const updatedRow: RowElement = { firstName: 'Guillaume', lastName: 'Salva', age: 23 };
+
+// Use exact pattern { ...row, age: 23 }
+const updatedRow: RowElement = { ...row, age: 23 };
 
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
